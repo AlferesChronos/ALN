76141faa365a47227940827e856da78b88af1ff0
Atellas23 <alex.batlle01@gmail.com>
2019-05-10 14:06:24 +0200

    update

--- C++/Càlcul de la inversa/inversa.cc
+++ C++/Càlcul de la inversa/inversa.cc
@@ -1,7 +1,18 @@
 #include <fstream>
 #include <vector>
-#
+#include <iostream>
+
+#define A "inputA"
+#define b "inputb"
 
 int main() {
-    
+    int n;
+    cin >> n;
+    ofstream of;
+    of.open(A,ofstream::out);
+    of.close();
+    of.open(b,ofstream::out);
+    of << 1 << endl;
+    of << 
+    of.close();
 }
