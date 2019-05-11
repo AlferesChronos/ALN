6be173b834b3c07385416dba3496aa7d39717c21
Atellas23 <40756655+Atellas23@users.noreply.github.com>
2019-05-11 11:34:30 +0200

    Update residu.cc

--- C++/Càlcul de la inversa/residu.cc
+++ C++/Càlcul de la inversa/residu.cc
@@ -6,21 +6,24 @@ typedef vector<double> Vector;
 typedef vector<Vector> Matriu;
 
 /* Capçaleres de les funcions (estan implementades en altres arxius). */
-double res_2 (Vector& r, int n);
-double res_Inf (Vector& r, int n);
+double res_2 (Matriu& R, int n);
+double res_Inf (Matriu& R, int n);
+void product (const Matriu& A, const Matriu& B, Matriu& P);
 
-void residu (Matriu& A, Vector& x, Vector& b, int n, double res2, double resInf) {
 
-	// CÂLCUL DE r = Ax-b 
-	Vector r(n);
+void residu (Matriu& A, Matriu& X, Matriu& Id, int n, double res2, double resInf) {
+
+	// CÀLCUL DE R = AX-Id 
+	Matriu R(n, Vector(n));
+	Matriu P(n, Vector(n));
+	product(A,X,P);
 	for (int i = 0; i < n; ++i) {
-		r[i] = -b[i];
-		for (int j = 0; j < n; ++j) r[i] += A[i][j]*x[j];
+		for (int j = 0; j < n; ++j) R[i][j] = P[i][j]-Id[i][j];
 	}
 
 	// NORMA SUB2 DEL RESIDU
-	res2 = res_2(r, n);
+	res2 = res_2(R, n);
 
 	// NORMA SUBINFINIT DEL RESIDU
-	resInf = res_Inf(r, n);
+	resInf = res_Inf(R, n);
 }
