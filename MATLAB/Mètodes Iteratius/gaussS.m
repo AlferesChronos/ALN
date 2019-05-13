885f501a4980b3dee95cebf4322578b3b2b0dacd
Atellas23 <40756655+Atellas23@users.noreply.github.com>
2019-05-13 13:46:10 +0200

    Update gaussS.m

--- MATLAB/Mètodes Iteratius/gaussS.m
+++ MATLAB/Mètodes Iteratius/gaussS.m
@@ -1,17 +1,25 @@
-function [x,res,iter] = gaussS(A,b,x0,nmax,tol)
+function [x,rho,res,iter] = gaussS(A,b,x0,nmax,prec)
 
-P=diag(diag(A))+tril(A,-1);
+tol=1.0e-12;
+D=diag(A);
+if min(abs(D))<tol %check for zeros on the diagonal
+    error('error: valors a la diagonal amb valor absolut < %e',tol)
+end
+
+x=x0(:);
+b=b(:);
+
+P=tril(A);
 n=size(P,1);
 B=-P\triu(A,1);
 c=P\b;
-x=x0;
+rho=max(abs(eig(B)));
 
-for i=1:nmax
+for iter=1:nmax
     x=B*x+c;
     res=norm(A*x-b);
-    iter=i;
-    if res < tol
-        break
+    if res < prec
+        return
     end
 end
 
