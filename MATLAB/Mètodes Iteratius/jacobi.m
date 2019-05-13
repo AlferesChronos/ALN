c128dcec0411d431763e71fd2b70c19acd093e3c
Atellas23 <40756655+Atellas23@users.noreply.github.com>
2019-05-13 13:54:41 +0200

    Update jacobi.m

--- MATLAB/Mètodes Iteratius/jacobi.m
+++ MATLAB/Mètodes Iteratius/jacobi.m
@@ -1,12 +1,19 @@
 function [x,res,iter] = jacobi(A,b,x0,nmax,tol)
 
+tol=1.0e-12;
+D=diag(A);
+if min(abs(D))<tol %check for zeros on the diagonal
+    error('error: valors a la diagonal amb valor absolut < %e',tol)
+end
+
 P=diag(diag(A));
 n=size(P,1);
 B=eye(n)-P\A;
+b=b(:);
 c=P\b;
-x=x0;
+x=x0(:);
 
-for i=1:nmax
+for iter=1:nmax
     x=B*x+c;
     res=norm(A*x-b);
     iter=i;
