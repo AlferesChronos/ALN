ffc619af06a9f7afaa7a905705e6648a95c24b90
Atellas23 <40756655+Atellas23@users.noreply.github.com>
2019-05-13 13:56:43 +0200

    Update overRelaxation.m

--- MATLAB/Mètodes Iteratius/overRelaxation.m
+++ MATLAB/Mètodes Iteratius/overRelaxation.m
@@ -3,13 +3,14 @@ function [x,res,iter] = overRelaxation(A,b,x0,w,nmax,tol)
 P=(1/w)*diag(diag(A))+tril(A,-1);
 n=size(P,1);
 B=P\(P-A);
+b=b(:);
 c=P\b;
-x=x0;
+x=x0(:);
+rho=max(abs(eig(B)));
 
-for i=1:nmax
+for iter=1:nmax
     x=B*x+c;
     res=norm(A*x-b);
-    iter=i;
     if res < tol
         break
     end
