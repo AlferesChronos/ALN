f3272ad6b941e15e4723682c8172b3d7cfa8ba97
Atellas23 <40756655+Atellas23@users.noreply.github.com>
2019-05-14 19:28:39 +0200

    Update DiagDet.m

--- MATLAB/DiagDet.m
+++ MATLAB/DiagDet.m
@@ -1,7 +1,10 @@
-function [ det ] = DiagDet( R )
-% Determinant d'una matriu diagonal (o triangular)
+function [det] = DiagDet(R)
+% DIAGDET   Aquesta funció calcula el determinant d'una matriu diagonal (o triangular)
+% R mitjançant el producte d'elements de la diagonal.
+
 n = size(R);
 det = 1;
+
 for i = 1:n
     a = R(i, i);
     if a == 0
@@ -10,4 +13,5 @@ for i = 1:n
     end
     det = det*a;
 end
+
 end
